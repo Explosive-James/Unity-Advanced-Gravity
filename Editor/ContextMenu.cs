12a3a7d76e49672ae68b43de1d5787b1ec8be683
James <28958050+Explosive-James@users.noreply.github.com>
2022-03-07 19:46:01 +0000

    Added create context menu
     + Added gravity fields to the game object context menu to make it easier to create new gravity fields.

---
+++ Editor/ContextMenu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+using AdvancedGravity;
+
+namespace AdvancedGravityEditor
+{
+    public class ContextMenu
+    {
+        [MenuItem("GameObject/GravityFields/Spherical Gravity")]
+        static void CreateSphericalGravity(MenuCommand menuCommand) => CreateGravityField(menuCommand, typeof(SphericalGravity));
+
+        [MenuItem("GameObject/GravityFields/Cubic Gravity")]
+        static void CreateCubicGravity(MenuCommand menuCommand) => CreateGravityField(menuCommand, typeof(CubicGravity));
+
+        [MenuItem("GameObject/GravityFields/Planar Gravity")]
+        static void CreatePlanarGravity(MenuCommand menuCommand) => CreateGravityField(menuCommand, typeof(PlanarGravity));
+
+        [MenuItem("GameObject/GravityFields/Cylindrical Gravity")]
+        static void CreateCylindricalGravity(MenuCommand menuCommand) => CreateGravityField(menuCommand, typeof(CylindricalGravity));
+
+        [MenuItem("GameObject/GravityFields/Capsule Gravity")]
+        static void CreateCapsuleGravity(MenuCommand menuCommand) => CreateGravityField(menuCommand, typeof(CapsuleGravity));
+
+        [MenuItem("GameObject/GravityFields/Donut Gravity")]
+        static void CreateDonutGravity(MenuCommand menuCommand) => CreateGravityField(menuCommand, typeof(DonutGravity));
+
+        static void CreateGravityField(MenuCommand command, Type gravityType)
+        {
+            GameObject go = new GameObject(gravityType.Name);
+            GameObjectUtility.SetParentAndAlign(go, command.context as GameObject);
+
+            go.AddComponent(gravityType);
+
+            Undo.RegisterCreatedObjectUndo(go, "Create " + go.name);
+            Selection.activeGameObject = go;
+        }
+    }
+}
